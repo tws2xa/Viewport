dde9a98b95a893c31bc72fd17e2c3d315a4b57e7
tws2xa <tws2xa@virginia.edu>
2015-09-09 12:36:21 -0400

    Replaced DestroyOnPlayerCollision.cs script
    Instead, have DestroyOnCollision.cs, which takes in a list of tags that
    indicate whether or not the object should be destroyed.

---
+++ Viewport/Assets/Scripts/DestroyOnCollision.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DestroyOnCollision : MonoBehaviour {
+
+	/// <summary>
+	/// This list contains all destroy trigger tags.
+	/// If the object collides with anything that has one
+	/// of these tags, it will be destroyed. Otherwise, it
+	/// will not be destroyed.
+	/// </summary>
+	public List<string> destroyTags = new List<string>();
+
+	// Called when the object collides with something.
+	void OnCollisionEnter(Collision collision) {
+		if (destroyTags.Contains(collision.gameObject.tag)) {
+			Destroy (this.gameObject);
+		}
+	}
+
+}
