899dc8bd91b97d5653e758b5e3f7ed9086479572
gtg3vv <gtg3vv@virginia.edu>
2015-10-17 13:32:46 -0400

    Main Menu
    Included some fonts

---
+++ Viewport/Assets/Scripts/ButtonBehavior.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnMouseEnter()
+    {
+        gameObject.GetComponent<Renderer>().material.color = Color.red;
+    }
+    void OnMouseExit()
+    {
+        gameObject.GetComponent<Renderer>().material.color = Color.blue;
+    }
+}
---
+++ Viewport/Assets/Scripts/ButtonOnclick.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonOnclick : MonoBehaviour {
+
+	public void changeScene(int scene)
+    {
+        Application.LoadLevel(scene);
+    }
+    public void endMenu()
+    {
+        Application.Quit();
+    }
+}
