29d2e57ecd38fff2f1bba58bcee725062e40619c
gtg3vv <gtg3vv@virginia.edu>
2015-10-17 15:08:48 -0400

    Menu Fixed

---
+++ Viewport/Assets/Scripts/menuReturn.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menuReturn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.LoadLevel(0);
+	}
+}
