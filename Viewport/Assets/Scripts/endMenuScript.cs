0014eb28832624103a0ae065e9400e5ff3733c98
enwint <enwinters@comcast.net>
2015-12-05 13:56:42 -0500

    endMenu shanges

--- Viewport/Assets/Scripts/endMenuScript.cs
+++ Viewport/Assets/Scripts/endMenuScript.cs
@@ -6,17 +6,27 @@ using UnityEngine;
 using System.Collections.Generic;
 
 public class endMenuScript : MonoBehaviour {
-	/*
+	//time texts: p1t is the winner time -> p4t is the loser time
+
+
+	public List<Text> playerTimeText = new List<Text>();
+	
+	public List<Text> placingText = new List<Text>();
+
+	public List<Text> playerPlaceText = new List<Text>();
+
+
 
 	void Start () {
-
+		setTimes ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		setTimes ();
 	}
 
-	public static void setMenu() {
+	public void setTimes() {
 		//get players
 		//generate 2, 3 or 4 places
 		//display lowest time first
@@ -29,74 +39,62 @@ public class endMenuScript : MonoBehaviour {
 
 		GameObject[] players = GameObject.FindGameObjectsWithTag ("Player");
 		GameObject[] sortPlayers;
+		List<float> times = new List<float>();
+		Dictionary<float, int> dictionary =
+			new Dictionary<float, int>();
 
-			if (PlayerPrefs.HasKey ("p1TimeActive")) {
-				float player1Time = PlayerPrefs.GetFloat ("p1TimeActive");
-			}
-			if (PlayerPrefs.HasKey ("p2TimeActive")) {
-				float player2Time = PlayerPrefs.GetFloat ("p2TimeActive");
-			}
-			if (PlayerPrefs.HasKey ("p3TimeActive")) {
-				float player3Time = PlayerPrefs.GetFloat ("p3TimeActive");
-			}
-			if (PlayerPrefs.HasKey ("p4TimeActive")) {
-				float player4Time = PlayerPrefs.GetFloat ("p4TimeActive");
-			}
+		dictionary.Add (3f, 1);
+		dictionary.Add (2f, 2);
+		dictionary.Add (31f, 3);
+		dictionary.Add (4f, 4);
 
-		//sort by times
+		/*
+		if (PlayerPrefs.HasKey ("p1TimeActive")) {
+			float player1Time = PlayerPrefs.GetFloat ("p1TimeActive");
 
+
+			dictionary.Add(player1Time, 1);
+		}
+		if (PlayerPrefs.HasKey ("p2TimeActive")) {
+			float player2Time = PlayerPrefs.GetFloat ("p2TimeActive");
+		dictionary.Add(player2Time, 2);
+		}
+		if (PlayerPrefs.HasKey ("p3TimeActive")) {
+			float player3Time = PlayerPrefs.GetFloat ("p3TimeActive");
+			
+
+		dictionary.Add(player3Time, 3);
+
+		}
+		if (PlayerPrefs.HasKey ("p4TimeActive")) {
+			float player4Time = PlayerPrefs.GetFloat ("p4TimeActive");
+		dictionary.Add(player4Time, 4);
+		}
+		*/
 	
 
-		if (players.Length==4) {
-		
-				int tmp;
-				if (player1Time > player2Time) { tmp = player1Time; player1Time = player2Time; player2Time = tmp; }
-				if (player3Time > player4Time) { tmp = player3Time; player3Time = player4Time; player4Time = tmp; }
-				if (player1Time > player3Time) { tmp = player1Time; player1Time = player3Time; player3Time = tmp; }
-				if (player2Time > player4Time) { tmp = player2Time; player2Time = player4Time; player4Time = tmp; }
-				if (player2Time > player3Time) { tmp = player2Time; player2Time = player3Time; player3Time = tmp; }
 
-			Text player1T = GameObject.Find("ScoreMenu").GetComponent<p1time>();
-			Text player2T = GameObject.Find("ScoreMenu").GetComponent<p2time>();
-			Text player3T = GameObject.Find("ScoreMenu").GetComponent<p3time>();
-			Text player4T = GameObject.Find("ScoreMenu").GetComponent<p4time>();
-			
-			player1T.text = player1Time;
-			player2T.text = player2Time;
-			player3T.text = player3Time;
-			player4T.text = player4Time;
-
-			}
-
-		if(players.Length == 3) {
-
-
-
-
-			Text player1T = GameObject.Find("ScoreMenu").GetComponent<p1time>();
-			Text player2T = GameObject.Find("ScoreMenu").GetComponent<p2time>();
-			Text player3T = GameObject.Find("ScoreMenu").GetComponent<p3time>();
-			
-			player1T.text = player1Time;
-			player2T.text = player2Time;
-			player3T.text = player3Time;
-		
-			}
-		if(players.Length == 2){
-				if(player1Time > player2Time) {
-						maxTime = player1Time;
-					}
-					else {
-						maxTime = player2Time;
-					}
-			Text player1T = GameObject.Find("ScoreMenu").GetComponent<p1time>();
-			Text player2T = GameObject.Find("ScoreMenu").GetComponent<p2time>();
-
-			player1T.text = player1Time;
-			player2T.text = player2Time;
-						
+	foreach (KeyValuePair<float, int> pair in dictionary)
+		    {
+			times.Add(pair.Key);
 		}
+
+
+		//p1ball getInt 
+		//textureChange.cs
+ 
+		times.Sort ();
+		times.Reverse ();
+
+		for (int i = 0; i < times.Count; i++) {
+			this.playerTimeText[i].text = times[i].ToString ();
+			int playerNumber = dictionary[times[i]];
+			string playerNumberStr = "Player " + playerNumber;
+			this.playerPlaceText[i].text = playerNumberStr;
+
+			}
+
+			
 	
-
-	*/
-		}
+	}	
+}
